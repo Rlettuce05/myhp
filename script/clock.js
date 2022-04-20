445fc3847eb64cc1222513d238180315bfdb8b60
syunsuke okamura <syunrobin@gmail.com>
2022-04-21 00:29:19 +0900

    ✒ clock.js : 2 arc filled

--- script/clock.js
+++ script/clock.js
@@ -2,8 +2,13 @@ function setup() {
     var canvas = document.getElementById("ClockPlace");
     if (canvas.getContext) {
         var context = canvas.getContext("2d");
-        context.fillstyle = "rgb(180, 225, 151)";
+        context.fillStyle = "rgb(180, 225, 151)";
+        context.beginPath();
         context.arc(250, 250, 250, 0, Math.PI * 2, true);
+        context.fill();
+        context.fillStyle = "rgb(233, 239, 192)";
+        context.beginPath();
+        context.arc(250, 250, 200, 0, Math.PI * 2, true);
         context.fill();
     }
 }
