2b0f0b11239ff552d996261925571296e38e5818
syunsuke okamura <syunrobin@gmail.com>
2022-04-20 23:41:31 +0900

    🔧 clock.js :  same as tutorial

--- script/clock.js
+++ script/clock.js
@@ -1,5 +1,6 @@
-var context = document.getElementById("ClockPlace").getContext("2d");
-if (context) {
-    context.fillstyle = "rgb(180, 225, 151)"
-    context.arc(75, 75, 50, 0, Math.PI * 2);
+var canvas = document.getElementById("#ClockPlace");
+if (canvas.getContext) {
+    var context = canvas.getContext("2d");
+    context.fillstyle = "rgb(180, 225, 151)";
+    context.arc(75, 75, 50, 0, Math.PI * 2, true);
 }
