9b6f593b5334dc5b51ce80c3e11c3d4068d03af1
syunsuke okamura <syunrobin@gmail.com>
2022-04-21 00:15:27 +0900

    🔧 clock.js : draw arc

--- script/clock.js
+++ script/clock.js
@@ -1,8 +1,9 @@
 function setup() {
-    var canvas = document.getElementById("#ClockPlace");
+    var canvas = document.getElementById("ClockPlace");
     if (canvas.getContext) {
         var context = canvas.getContext("2d");
         context.fillstyle = "rgb(180, 225, 151)";
-        context.arc(75, 75, 50, 0, Math.PI * 2, true);
+        context.arc(250, 250, 250, 0, Math.PI * 2, true);
+        context.fill();
     }
 }
