0990dcf30b61e08634f59f6f26fc0b0f74439392
syunsuke okamura <syunrobin@gmail.com>
2022-04-21 01:04:31 +0900

    ✒ clock.js : dial(on the way)

--- script/clock.js
+++ script/clock.js
@@ -10,5 +10,16 @@ function setup() {
         context.beginPath();
         context.arc(250, 250, 200, 0, Math.PI * 2, true);
         context.fill();
+        context.fillStyle = "rgb(78, 148, 79)";
+        context.font = "45px Akshar"
+        context.beginPath();
+        context.fillText("12", 235, 45);
+        context.fillText("1", 355, 75);
+        context.fillText("2", 425, 150);
+        context.fillText("3", 455, 250);
+        context.fillText("4", 425, 370);
+        context.fillText("5", 358, 452);
+        context.fillText("6", 250, 485);
+        context.fillText("9", 25, 255);
     }
 }
