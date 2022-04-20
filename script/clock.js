fc4abb8af2afb3bc7d55b72f1dd02c352e0d99f2
syunsuke okamura <syunrobin@gmail.com>
2022-04-20 23:47:29 +0900

    🔧 clock.html, ~.js : call setup(draw) onload

--- script/clock.js
+++ script/clock.js
@@ -1,6 +1,8 @@
-var canvas = document.getElementById("#ClockPlace");
-if (canvas.getContext) {
-    var context = canvas.getContext("2d");
-    context.fillstyle = "rgb(180, 225, 151)";
-    context.arc(75, 75, 50, 0, Math.PI * 2, true);
+function setup() {
+    var canvas = document.getElementById("#ClockPlace");
+    if (canvas.getContext) {
+        var context = canvas.getContext("2d");
+        context.fillstyle = "rgb(180, 225, 151)";
+        context.arc(75, 75, 50, 0, Math.PI * 2, true);
+    }
 }
