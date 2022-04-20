5473ed9eaf928f1f5d748711716286aba1df19a1
syunsuke okamura <syunrobin@gmail.com>
2022-04-20 23:34:58 +0900

    🔧 clock.html, ~.js : not use jq

--- script/clock.js
+++ script/clock.js
@@ -1,7 +1,5 @@
-$(() => {
-    var context = $("#ClockPlace").getContext("2d");
-    if (context) {
-        context.fillstyle = "rgb(180, 225, 151)"
-        context.arc(75, 75, 50, 0, Math.PI * 2, true);
-    }
-})
+var context = document.getElementById("ClockPlace").getContext("2d");
+if (context) {
+    context.fillstyle = "rgb(180, 225, 151)"
+    context.arc(75, 75, 50, 0, Math.PI * 2);
+}
